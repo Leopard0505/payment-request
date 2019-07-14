89ee5fbc43e07c7b28eaeaf3116db0b80d3407fa
Leopard0505 <k.9975hyo5@gmail.com>
2019-07-14 19:45:05 +0900

    Update test

---
+++ test/index.test.js
@@ -0,0 +1,36 @@
+import PaymentRequestMethod from '../index'
+
+const params = {
+  url: '/pay',
+  stripe_pk_key: 'public_stripe_pk_key'
+}
+const displayItems = [
+  {
+    label: 'First Item',
+    amount: { currency: 'JPY', value: '1000' }
+  },
+  {
+    label: 'Second Item',
+    amount: { currency: 'JPY', value: '-200' }
+  }
+]
+
+describe('PaymentRequestMethod', () => {
+  const payment = new PaymentRequestMethod(params, displayItems)
+  describe('constructor()', () => {
+    it('constructor', () => {
+      expect(payment.url).toBe('/pay')
+      expect(payment.stripe_pk_key).toBe('public_stripe_pk_key')
+    })
+  })
+
+  describe('calculation()', () => {
+    const { amount } = payment.calculation(displayItems)
+    test('amount#currency', () => {
+      expect(amount.currency).toBe('JPY')
+    })
+    test('amount#value', () => {
+      expect(amount.value).toBe(800)
+    })
+  })
+})
